655d1f638cb50981a977b403f0e286c3dc998f94
-MM- <107755486+HH-GAME-MM@users.noreply.github.com>
2023-05-11 20:40:26 +0200

    v0.7

--- HH-Simulate-Headband-in-Pantheon.user.js
+++ HH-Simulate-Headband-in-Pantheon.user.js
@@ -1,12 +1,13 @@
 // ==UserScript==
 // @name         HH Simulate Headband in Pantheon
-// @version      0.6
+// @version      0.7
 // @description  Simulate wearing a Headband in Pantheon
 // @author       -MM-
 // @match        https://*.hentaiheroes.com/pantheon-pre-battle.html?id_opponent=*
 // @match        https://nutaku.haremheroes.com/pantheon-pre-battle.html?id_opponent=*
 // @match        https://www.comixharem.com/pantheon-pre-battle.html?id_opponent=*
 // @match        https://www.pornstarharem.com/pantheon-pre-battle.html?id_opponent=*
+// @match        https://www.gayharem.com/pantheon-pre-battle.html?id_opponent=*
 // @run-at       document-end
 // @namespace    https://github.com/HH-GAME-MM/HH-Simulate-Headband-in-Pantheon
 // @updateURL    https://github.com/HH-GAME-MM/HH-Simulate-Headband-in-Pantheon/raw/main/HH-Simulate-Headband-in-Pantheon.user.js
