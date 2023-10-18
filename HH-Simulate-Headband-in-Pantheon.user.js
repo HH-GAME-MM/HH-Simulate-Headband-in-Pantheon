676406697c4505ea6e6ee2606181e7ebac9bb67b
-MM- <107755486+HH-GAME-MM@users.noreply.github.com>
2023-10-18 18:10:14 +0200

    v0.8

--- HH-Simulate-Headband-in-Pantheon.user.js
+++ HH-Simulate-Headband-in-Pantheon.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         HH Simulate Headband in Pantheon
-// @version      0.7
+// @version      0.8
 // @description  Simulate wearing a Headband in Pantheon
 // @author       -MM-
 // @match        https://*.hentaiheroes.com/pantheon-pre-battle.html?id_opponent=*
@@ -22,11 +22,20 @@
     hero_data.damage *= 1.25;
 
     setTimeout(function() {
-        let span = document.querySelector('.pantheon-team-panel.player-panel .player_stats #stats-damage');
-        if(span != null)
-        {
+        const span = document.querySelector('.pantheon-team-panel.player-panel .player_stats #stats-damage');
+        if(span !== null) {
             span.setAttribute('style', 'color:rgb(255, 47, 47);');
             span.innerHTML = number_format_lang(hero_data.damage, 0);
+        }
+
+        setTimeout(addWarning, 1, 0);
+        function addWarning(i) {
+            const div = document.querySelector('.pantheon-team-panel.opponent-panel .matchRating');
+            if(div !== null) {
+                div.innerHTML += '<div style="position:absolute;margin-top:-70px;width:120px;font-size:14px;color:#ff2f2f;" tooltip="Make sure that you do not use the headband script while using a real headband. Otherwise your attack is increased twice!">Headband Script is enabled</div>';
+            } else {
+                if(i < 100) setTimeout(addWarning, 10, i + 1);
+            }
         }
     }, 1);
 })();
